0cbaeca332caf1f9243e7e9e5a39c15b7d867965
unknown <flysparky@cox.net>
2018-06-16 20:07:15 -0700

    done with Trilogy

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+    <footer className="footer text-center">
+        &copy; Andrew Nowlin
+    </footer>
+);
+
+export default Footer;
